3e28d85f126299934f42f3779c30e1c4e3e08288
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2023-04-16 19:56:08 +0700

    Урок 2. Циклы (for, while)

--- task_2.py
+++ task_2.py
@@ -1,32 +1,15 @@
 """
-    Задача 10: 
+    Задача 2. 
     
-    На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной. Выведите минимальное количество монет, которые нужно перевернуть
+    Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z.
 """
-import random
 
-from random import randint
-
-amount_coin = int(input('введите количество монет: '))
-
-m = 0
-k = 0
-coins = [0, 1]
-for i in range(amount_coin):
-    random.shuffle(coins)
-    print(f'все монеты{coins}')
-    if int(coins[0]) == 0:
-        k += 1
-    if int(coins[0]) == 1:
-        m += 1
-print(f'всего монет {m, k}')
-
-if m > k:
-    ans = k
-else:
-    ans = m
-
-print(f"минимальное количество монет, которые нужно перевернуть {ans}")
+print('¬(X ∧ Y) ∨ Z')
+for x in range (0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            result = not(x and y) or z
+            print(f'{x} | {y} | {z} | {int(not x or y or z)}')
 
 
 

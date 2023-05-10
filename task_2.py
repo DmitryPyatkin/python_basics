a913caff3b2f2e2e4b751817048a0583ea133109
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2023-05-10 20:47:14 +0700

    Урок 2. Коллекции данных. Профилирование и отладка

--- task_2.py
+++ task_2.py
@@ -3,13 +3,13 @@
     
     Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z.
 """
-
-print('¬(X ∧ Y) ∨ Z')
-for x in range (0, 2):
-    for y in range(0, 2):
-        for z in range(0, 2):
-            result = not(x and y) or z
-            print(f'{x} | {y} | {z} | {int(not x or y or z)}')
+def task_2():
+    print('¬(X ∧ Y) ∨ Z')
+    for x in range (0, 2):
+        for y in range(0, 2):
+            for z in range(0, 2):
+                print(f'{x} | {y} | {z} | {int(not (x or y) and z)}')
+task_2()
 
 
 

09a9bd6fc5bb4d69409755db420c56953f2c39bc
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2023-05-10 20:34:58 +0700

    Урок 2. Коллекции данных. Профилирование и отладка

--- task_1.py
+++ task_1.py
@@ -4,17 +4,29 @@
     Напишите программу, которая принимает на вход число N и выдает список факториалов для чисел от 1 до N.
     пусть N = 4 -> [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 """
-def task_1(): 
-    number = int(input('Введите число: '))
-    print(f'{number}! -> ', end='[ ')
-    factorial = 0
-    if number == 0:
-        print('0! = 0')
-    else:
-        factorial = 1
-        for i in range(1, number + 1):
-            factorial *= i
-            print(factorial, end=', ' )
-    print(end=']')
-task_1()
+# def task_1(): 
+#     number = int(input('Введите число: '))
+#     print(f'{number}! -> ', end='[ ')
+#     factorial = 0
+#     if number == 0:
+#         print('0! = 0')
+#     else:
+#         factorial = 1
+#         for i in range(1, number + 1):
+#             factorial *= i
+#             print(factorial, end=', ')
+#     print(end=']')
+# task_1()
+
+# def fact(number):
+#     res = 1
+#     for n in range(1, number):
+#         res *= n
+#     return res
+
+# def task_1():
+#     number = int(input('Введите число: '))
+#     for n in range(1, number + 1):
+#         print(f'{n}! = {fact(n)}')
+# task_1()
 

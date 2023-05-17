0ffafaade04cafd2c4e9014de30061c3f4c05f10
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2023-05-17 20:05:37 +0700

    Урок 6. Повторение списков

--- task_1.py
+++ task_1.py
@@ -6,6 +6,18 @@
     N = 132:132 + 132132 + 132132132 = 132264396
 """
 
+n = 132
+result = n + n * 2 + n * 3 
+print(result) # -> 738
+
+n = '132'
+result = n + n * 2 + n * 3 
+print(result) # -> 123 123123 123123123
+
+n = '132'
+result = int(n) + int(n * 2) + int(n * 3) 
+print(result) # -> 132264396
+
 def task_1():
     n = input('Введите число: ')
     m = int(input('Введите количество преобразований числа: '))

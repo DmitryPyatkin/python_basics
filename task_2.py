7b2541247d59a1530f4e565b3a21388d156d2917
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2023-05-17 20:55:20 +0700

    Урок 6. Повторение списков

--- task_2.py
+++ task_2.py
@@ -6,25 +6,25 @@
     [4, 4, 3, 6, 7, 0, 8, 5, 1, 2] - 812 -> нет
 """
 import random
-# def task_2():
-#     massiv = [random.randint(0, 9) for _ in range(15)]
-#     print(massiv)
-#     number = input('Введите число: ')
+def task_2():
+    massiv = [random.randint(0, 9) for _ in range(15)]
+    print(massiv)
+    number = input('Введите число: ')
 
-#     res = []
-#     for el in number:
-#         res.append(int(el))
+    res = []
+    for el in number:
+        res.append(int(el))
     
-#     result = False
-#     for i in range(len(massiv) - len(res)):
-#         if res == massiv[i:len(res) + i]:
-#             result = True
+    result = False
+    for i in range(len(massiv) - len(res)):
+        if res == massiv[i:len(res) + i]:
+            result = True
     
-#     if result == True:
-#         print(f'{massiv} - {res} -> да')
-#     else:
-#         print(f'{massiv} - {res} -> нет')
-# task_2()
+    if result == True:
+        print(f'{massiv} - {res} -> да')
+    else:
+        print(f'{massiv} - {res} -> нет')
+task_2()
 
 numbers = [random.randint(0, 9) for _ in range(15)]
 print(numbers)
